c5f8d4096a4383359de78fa0c6cb84e0935e8024
Faisal Qayumi <faisal.qayumi88@gmail.com>
2016-08-29 07:47:00 -0400

    new Circle and Circle App class

--- Circle.java
+++ Circle.java
@@ -1,16 +1,49 @@
 import java.lang.Math;
+
 public class Circle {
-    public static double circle(double radius) 
-    { // takes input
-        double getArea = Math.PI * Math.pow( radius, 2 ); //.00
-        double getCircumference = 2 * Math.PI * radius; //.00
-        return getArea + getCircumference;
+	private double radius;
+	private static int ObjectCount;
+	
+	public Circle (double r){  //constructor
+		radius = r;
+		
+		ObjectCount++;
+	}
+		private String formatNumber(double x) {
+			Double.toString(x);
+			String result = String.format("%.2f", x);
+			return result;
+		}
 
-        
+		public double getCircumference() {
+			double circumference = 2 * radius * Math.PI ;
+			return circumference;
+		}
+		
+		public String getFormattedCircumference() {
+			double a = (getCircumference());
+			return formatNumber(a);
+		}
+		
+		public double getArea() {
+			double area = Math.PI * radius * radius;
+			return area;
+		}
+		
+		public String getFormattedArea() {
+			double a = (getArea());
+			return formatNumber(a);
+		}
+		
+		
+		
+		public int getObjectCount() {
+			return ObjectCount;
+		}
+		
+				
+}
 
-    }    
-
-    }    
-
-
+	
+	
 		
--- CircleApp.java
+++ CircleApp.java
@@ -1,60 +1,31 @@
-
-import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.lang.Math;
-import java.text.DecimalFormat;
-public class CircleApp {    
-	public static void main(String[] args) {
 
-        int counter = 0;
-        boolean x = true;
-        while (x) {
-             try {
+public class CircleApp {
 
-                System.out.println("Enter a radius for a circle (ft): ");
-                Scanner scan = new Scanner(System.in);
-                double intake1 = scan.nextDouble();
-                double outtake2 = CircleApp.getArea(intake1);
-                DecimalFormat dFormat1 = new DecimalFormat("#.00");
-                String outtake3 = dFormat1.format(outtake2); //had to change to String
-                double outtake6 = CircleApp.getCircumference(intake1);
-                String outtake7 = dFormat1.format(outtake6);
-                System.out.println("Area = " + outtake3 + " square ft.");
-                System.out.println("Circumference = " + outtake7 + " ft.");
-                System.out.println("Do you want to try a different circle? Yes/No?");
-                System.out.println( " "); //garbage problem
-                String finalFeedback = scan.nextLine();
-                System.out.println();
-                if (finalFeedback.equalsIgnoreCase("yes")) {
-                         x = true;
-                    } else x = false;
-                            scan.close();
-                    counter = counter++;
+	public static void main (String [ ] args) {
+		double radius = 0;
+		int count = 0;
+		String choice = "y";
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome to the Circle Tester!");
+		while(choice.equalsIgnoreCase("y")) 
+		{
+			System.out.println("Enter radius of circle: ");
+			radius = scan.nextDouble();
+			Circle circle = new Circle(radius);
 
-            } catch (IllegalArgumentException one) {
+			System.out.println("Circumference: " + circle.getFormattedCircumference());
+			System.out.println("Area: " + circle.getFormattedArea());
+			
+			count = circle.getObjectCount();
+			
+			System.out.println("create another Circle? (y/n): ");
+			choice = scan.next();
+		}
+			
+			System.out.println("You created " + count + " objects " + "Good-Bye");
 
-            System.out.println("Try again.");
-
-            } catch (InputMismatchException two) { //had to import this exception
-
-            System.out.println("Something doesn't add up. Try again.");
-
-            } finally {
-
-            }
-
-        }
-
-        System.out.println( counter + " total circles calculated. Bye!"); //count revolutions     
-    }
-
-    public static double getArea(double radius) {
-        double getArea1 = Math.PI * Math.pow( radius, 2 ); //.00
-        return getArea1;
-    }    
-    public static double getCircumference(double radius) {    
-
-        double getCircumference1 = 2 * Math.PI * radius; //.00
-            return getCircumference1;
-    }    
-}    
+			scan.close();
+	}
+}
